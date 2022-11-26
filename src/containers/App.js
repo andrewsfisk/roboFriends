a59dbe9a2c64ca313d44cd866726ee7db36481c9
andrewsfisk <andrewsfisk@gmail.com>
2022-11-26 16:13:37 +1100

    initial commit

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,46 +1,40 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundary from '../components/errorBoundary';
 import './App.css';
 
-class App extends Component {
-	constructor() {
-		super()
-		this.state = {
-			robots: [],
-			searchfield: ''
-		}
-	}
+function App() {
+const [robots, setRobots] = useState([])
+const [searchfield, setSearchfield] = useState('')
 
-	componentDidMount() {
-		fetch('https://jsonplaceholder.typicode.com/users')
-			.then(response=> response.json())
-			.then(users => this.setState({robots: users}));	
-	}
+useEffect(()=> {
+	fetch('https://jsonplaceholder.typicode.com/users')
+	.then(response => response.json())
+	.then(users => {setRobots(users)});
+},[])
 
-	onSearchChange = (event) => {
-		this.setState({ searchfield: event.target.value })
-	}
+const onSearchChange = (event) => {
+	setSearchfield(event.target.value)
+}
 
-	render() {
-		const filteredRobots = this.state.robots.filter(robot => {
-			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-		})
-		if (this.state.robots.length === 0) {
-			return <h1>Loading...</h1>
-		} else {
-			return (
-			<div className='tc'>
-				<h1 className='f1'>RoboFriends</h1>
-				<SearchBox searchChange={this.onSearchChange} />
-				<Scroll>
+const filteredRobots = robots.filter(robot => {
+	return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+})
+	return !robots.length ?
+		<h1>Loading...</h1> :
+	(
+		<div className='tc'>
+			<h1 className='f1'>RoboFriends</h1>
+			<SearchBox searchChange={onSearchChange} />
+			<Scroll>
+				<ErrorBoundary>
 					<CardList robots={filteredRobots} />
-				</Scroll>
-			</div>
-			);
-		}	
-	}
+				</ErrorBoundary>
+			</Scroll>
+		</div>
+	);
 }
 
 export default App;
